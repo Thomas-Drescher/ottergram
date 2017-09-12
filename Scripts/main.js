d897c7d03a14db82dc539448e416cef2e57c8eaa
Tom Drescher <tmdrescher@yahoo.com>
2017-09-11 22:06:14 -0400

    add comment to main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -6,6 +6,9 @@ var HIDDEN_DETAIL_CLASS = 'hidden-detail';
 var TINY_EFFECT_CLASS = 'is-tiny';
 var ESC_KEY = 27;
 
+
+// Change the detail image and the detail image title.
+
 function setDetails(imageUrl, titleText) {
   'use strict';
   var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
@@ -32,7 +35,7 @@ function setDetailsFromThumb(thumbnail) {
 
 function addThumbClickHandler(thumb) {
   'use strict';
-  thumb.addEventListener('click', function (event){
+  thumb.addEventListener('click', function(event) {
     event.preventDefault();
     setDetailsFromThumb(thumb);
     showDetails();
@@ -42,6 +45,9 @@ function addThumbClickHandler(thumb) {
 function getThumbnailsArray() {
   'use strict';
   var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+
+  // convert NodeList returned from querySelectorAll to an array
+
   var thumbnailArray = [].slice.call(thumbnails);
   return thumbnailArray;
 }
@@ -56,14 +62,14 @@ function showDetails() {
   var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
   document.body.classList.remove(HIDDEN_DETAIL_CLASS);
   frame.classList.add(TINY_EFFECT_CLASS);
-  setTimeout(function(){
+  setTimeout(function() {
     frame.classList.remove(TINY_EFFECT_CLASS);
   }, 50);
 }
 
 function addKeyPressHandler() {
   'use strict';
-  document.body.addEventListener('keyup', function (event) {
+  document.body.addEventListener('keyup', function(event) {
     event.preventDefault();
     console.log(event.keyCode);
     if (event.keyCode === ESC_KEY) {
